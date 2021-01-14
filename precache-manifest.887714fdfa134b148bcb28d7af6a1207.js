b73b2ea1ee4f9681405be95297470e1dee8933c3
psexetrap <sanartyo@yandex.ru>
2021-01-15 01:50:26 +0300

    Updates

---
+++ precache-manifest.887714fdfa134b148bcb28d7af6a1207.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "724595583d3c3624b108fed010a3e90a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "608619cd350f0e307d97",
+    "url": "./static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "5eb8253179c74c15aa93",
+    "url": "./static/css/main.439b4a20.chunk.css"
+  },
+  {
+    "revision": "608619cd350f0e307d97",
+    "url": "./static/js/2.710c261b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.710c261b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5eb8253179c74c15aa93",
+    "url": "./static/js/main.44c9bf2a.chunk.js"
+  },
+  {
+    "revision": "fb54b6bed906a3e09b6a",
+    "url": "./static/js/runtime-main.54d55149.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
